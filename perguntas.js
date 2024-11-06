081df616e99ead67407f6f4583f0816ddb6ee620
KelvenTecnologia <00001094256031SP@al.educacao.sp.gov.br>
2024-11-06 08:33:50 -0300

    ...

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'quantos mundiais o corinthians tem?',
+    'O corinthians tem 2 mundiais 
+)
+
+criaCartao(
+ 
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
